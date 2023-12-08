7af9782d3bb3e835459e70f0da677bf19dfb8b50
liu99002 <liu99002@gmail.com>
2023-12-08 21:29:28 +0800

    Fill.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,21 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0
+(loop)
+@0
+D=M  
+@exit
+D;JLE
+@1
+D=M
+@2
+M=M+D
+@0
+M=M-1
+@loop
+0;JMP
+(exit)
+@exit
+0;JMP
